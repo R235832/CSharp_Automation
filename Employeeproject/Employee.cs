35557299e26f3395ea357f92a78351ff049fad11
rakeshro <rakeshroutjspofficial@gmail.com>
2022-12-26 16:21:52 +0530

    Employee

---
+++ Employeeproject/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeproject
+{
+    public class Employee
+    {
+        public int emplid;
+        public string emplname;
+        public double emplsalary;
+        public static string companyname;
+        public static string companylocation;
+    }
+}
---
+++ Employeeproject/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Employeeproject;
+
+namespace Maveric.Runner
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********EMPLOYEE DASHBOARD********");
+            Employee.companyname = "Maveric";
+            Employee.companylocation = "Pune";
+            Employee emp1=new Employee();
+            Employee emp2=new Employee();
+            Employee emp3=new Employee();
+            emp1.emplid = 101;
+            emp1.emplname = "Rakesh Rout";
+            emp1.emplsalary = 20000;
+            emp2.emplid = 102;
+            emp2.emplname = "Raghav kim";
+            emp2.emplsalary = 18000.8;
+            emp3.emplid = 103;
+            emp3.emplname = "Rishwa jack";
+            emp3.emplsalary = 1200.3;
+         Console.WriteLine( emp1.emplid);
+            Console.WriteLine(emp1.emplname);
+            Console.WriteLine(emp1.emplsalary);
+            Console.WriteLine(Employee.companyname);
+            Console.WriteLine(Employee.companylocation);
+            Console.WriteLine(emp2.emplid);
+            Console.WriteLine(emp2.emplname);
+            Console.WriteLine(emp2.emplsalary);
+            Console.WriteLine(Employee.companyname);
+            Console.WriteLine(Employee.companylocation);
+            Console.WriteLine(emp3.emplid);
+            Console.WriteLine(emp3.emplname);
+            Console.WriteLine(emp3.emplsalary);
+            Console.WriteLine(Employee.companyname);
+            Console.WriteLine(Employee.companylocation);
+
+
+        }
+    }
+}
