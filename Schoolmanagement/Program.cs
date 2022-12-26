f41f34113c1138b668933cb61eccfdd02afd96a1
rakeshro <rakeshroutjspofficial@gmail.com>
2022-12-26 23:15:13 +0530

    Student Data

---
+++ Schoolmanagement/Program.cs
@@ -0,0 +1,40 @@
+﻿using Schoolmanagement;
+
+namespace Maceric.School
+{
+public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------STUDENT DATA--------------------------");
+            Student.SchoolName = "Global School";
+            Student.SchoolAdress = "Chennai";
+            Student std1 = new Student();
+            Student std2 = new Student();
+            Student std3 = new Student();
+            std1.Studentid = 1001;
+            std1.StudentName = "Jack";
+            std1.StudentMailId = "jack@gmail.com";
+            std1.StudentPercentage = 45.2;
+          
+            std2.Studentid = 1002;
+            std2.StudentName = "Peter";
+            std2.StudentMailId = "peter@gmail.com";
+            std2.StudentPercentage = 85.2;
+         
+            std3.Studentid = 1003;
+            std3.StudentName = "Mark";
+            std3.StudentMailId = "mark@gmail.com";
+            std3.StudentPercentage = 56.5;
+            std1.PrintStudentDetails();
+            std2.PrintStudentDetails();
+            std3.PrintStudentDetails();
+            std1.DisplayCertification();
+            std2.DisplayCertification();
+            std3.DisplayCertification();
+
+
+        }
+    }
+
+}
---
+++ Schoolmanagement/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoolmanagement
+{
+    public class Student
+    {
+        public static string SchoolName;
+        public static string SchoolAdress;
+        public int Studentid;
+        public string StudentName;
+        public string StudentMailId;
+        public double StudentPercentage;
+      
+        public void PrintStudentDetails()
+        {
+            Console.WriteLine("The name of the school is : " + Student.SchoolName);
+            Console.WriteLine("The school address is : " + Student.SchoolAdress);
+            Console.WriteLine("The student id is :" + Studentid);
+            Console.WriteLine("The student name is : " + StudentName);
+            Console.WriteLine("The Email id Of the Student is : " + StudentMailId);
+            Console.WriteLine("The percentage of the student is : " + StudentPercentage);
+            Console.WriteLine("----------------------------------------------------------");
+
+        }
+        public void DisplayCertification()
+        {
+            Console.WriteLine("Stdent percentage is: " + StudentPercentage);
+            if (StudentPercentage > 90)
+            {
+                Console.WriteLine("Hi  "   +   StudentName + "!  You have successfully passed with grade A  ");
+            }
+            else if (StudentPercentage >= 80 && StudentPercentage < 90)
+            {
+                Console.WriteLine("Hi  "   +   StudentName + " !  You have successfully passed with grade A ");
+            }
+            else if (StudentPercentage >= 60 && StudentPercentage < 80)
+            {
+                Console.WriteLine("Hi  "   +   StudentName  + " !  You have successfully passed with grade C");
+
+            }
+            else if (StudentPercentage < 60)
+            {
+                Console.WriteLine("Hi  "    +   StudentName + "!  You have successfully passed with grade C");
+            }
+
+            
+        }
+    }
+}
