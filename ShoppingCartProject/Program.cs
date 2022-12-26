4c29b5343950416ec572a20a3e716fe655a078c0
rakeshro <rakeshroutjspofficial@gmail.com>
2022-12-26 23:58:31 +0530

    Shopping Details

---
+++ ShoppingCartProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using ShoppingCartProject;
+
+namespace Maveric.Shoppingcart
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------SHOPPING CART DETAILS-----------------------------");
+            Shopping shp1= new Shopping();
+            Shopping shp2= new Shopping();
+            Shopping shp3= new Shopping();
+            shp1.Fordetailsid = 100;
+            shp1.DescribeField = "Laptop";
+            shp1.Price = 40000;
+            shp1.Quantity = 3;
+            shp2.Fordetailsid = 101;
+            shp2.DescribeField = "Mobile";
+            shp2.Price = 28500.50;
+            shp2.Quantity = 2;
+            shp3.Fordetailsid = 103;
+            shp3.DescribeField = "TV";
+            shp3.Price = 24000.80;
+            shp3.Quantity = 5;
+            shp1.PrintItemDetails();
+            shp2.PrintItemDetails();
+            shp3.PrintItemDetails();
+            shp1.discountedprice();
+            shp2.discountedprice();
+            shp3.discountedprice();
+
+        }
+    }
+}
---
+++ ShoppingCartProject/Shopping.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartProject
+{
+    public class Shopping
+    {
+        public int Fordetailsid;
+        public string DescribeField;
+        public int Quantity;
+        public double Price;
+
+        public void PrintItemDetails()
+        {
+            Console.WriteLine("Product Id is : " + Fordetailsid);
+            Console.WriteLine("Product name : " + DescribeField);
+            Console.WriteLine("Quantity : " + Quantity);
+            Console.WriteLine("Price of the product : " + Price);
+            Console.WriteLine("-------------------------------------------");
+        }
+        public void discountedprice()
+        {
+            Console.WriteLine("Quantity is : " + Quantity);
+            if(Quantity==2)
+            {
+                Console.WriteLine("The Final Price is : " + (Price - (Price * 10 / 100))*Quantity);
+            }
+            else if(Quantity>=3 || Quantity<5)
+            {
+                Console.WriteLine("The Final Price is : " + (Price - (Price * 15 / 100)) * Quantity);
+            }
+            else if(Quantity==5)
+            {
+                Console.WriteLine("The Final Price is : " + (Price - (Price * 25 / 100)) * Quantity);
+            }
+            Console.WriteLine("-------------------------------------------");
+        }
+        }
+        }
+
+    
+
