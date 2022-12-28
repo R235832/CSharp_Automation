3128876746b612e12bc2eeca49ab4918dac2db8d
rakeshro <rakeshroutjspofficial@gmail.com>
2022-12-28 10:26:13 +0530

    Selenium Assighnment for Fcebooklogin and sales force login

---
+++ FacebookLoginProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstDaySeleniumassignment
+{
+    public class Facebookloginpage
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.facebook.com/signup";
+            Console.WriteLine(driver.Url);
+            Console.WriteLine(driver.Title);
+            driver.FindElement(By.Name("firstname")).SendKeys("john");
+            
+            driver.FindElement(By.Name("lastname")).SendKeys("wick");
+            
+            driver.FindElement(By.Name("reg_email__")).SendKeys("demo123@gmail.com");
+            driver.FindElement(By.Name("reg_email_confirmation__")).SendKeys("demo123@gmail.com");
+            driver.FindElement(By.Name("reg_passwd__")).SendKeys("welcome 123");
+            driver.FindElement(By.Name("birthday_day")).Click();
+           
+            driver.FindElement(By.Name("birthday_month")).Click();
+            driver.FindElement(By.Name("birthday_year")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("sex")).Click();
+            driver.FindElement(By.Name("websubmit")).Click();
+        }
+    }
+}
---
+++ SalesForceLoginPage/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstDaySeleniumAssignment
+{
+    public class Sales
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.salesforce.com/in/form/signup/freetrial-sales/";
+            Console.WriteLine(driver.Title);
+            Console.WriteLine(driver.Url);
+            driver.FindElement(By.Name("UserFirstName")).SendKeys("John");
+            driver.FindElement(By.Name("UserLastName")).SendKeys("Wick");
+            driver.FindElement(By.Name("UserEmail")).SendKeys("john@gmail.com");
+            driver.FindElement(By.Name("UserTitle")).Click();
+            driver.FindElement(By.Name("CompanyName")).SendKeys("Maveric");
+            driver.FindElement(By.Name("CompanyEmployees")).Click();
+            driver.FindElement(By.Name("CompanyCountry")).Click();
+            driver.FindElement(By.ClassName("checkbox-ui")).Click();
+            driver.FindElement(By.Name("start my free trial")).Click();
+
+        }
+    }
+}
