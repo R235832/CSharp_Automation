c5e2960b243833768b43181829b158548368fe2f
rakeshro <rakeshroutjspofficial@gmail.com>
2023-01-02 14:43:23 +0530

    Assignment(31-dec-2022)

---
+++ SeleniumAdvance/RedBus.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvance
+{
+    public class RedBus
+    {
+        [Test]
+        public void BookTicket()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://www.redbus.in/";
+            driver.FindElement(By.Id("i-icon-profile")).Click();
+            driver.FindElement(By.Id("signInLink")).Click();
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[@class='modalIframe']")));
+            driver.FindElement(By.Id("mobileNoInp")).SendKeys("7898");
+            string errorMessage = driver.FindElement(By.XPath("//span[@class='error-message-fixed error-message-full top-fix']")).Text;
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
